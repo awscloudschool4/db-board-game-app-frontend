1a42643d328105e549abe4ac367e195c4c78accd
Alice <jaeyeon0921@gmail.com>
2024-02-22 11:12:02 +0900

    Project Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Theme } from "@radix-ui/themes";
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import "@radix-ui/themes/styles.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DICE",
+  description: "Created by AWS Cloud School 4기 3조",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Theme>
+          <Navbar />
+          <main className="flex min-h-screen flex-col items-center justify-between px-10 md:px-24 py-24">
+            {children}
+          </main>
+        </Theme>
+      </body>
+    </html>
+  );
+}
